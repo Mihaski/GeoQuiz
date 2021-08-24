137ee807aa0fb9e6b256a3fc19df84d3f2c926bc
Mihaski <mzabrodin94@gmail.com>
2021-08-25 00:07:37 +0700

    add 1)предотвращение ввода нескольких ответов на один и тотже вопрос 2) вывод score

--- app/src/main/java/com/bignerdrancho/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdrancho/android/geoquiz/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 // import android.view.Gravity
-import android.view.View
+//import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -13,14 +13,18 @@ private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
     private lateinit var questionTextView:TextView
     private val questionBank = listOf(
-        Question(R.string.question_australia,true),
-        Question(R.string.question_oceans,true),
-        Question(R.string.question_mideast,false),
-        Question(R.string.question_africa,false),
-        Question(R.string.question_americas ,true),
-        Question(R.string.question_asia,true))
+        Question(R.string.question_australia,true,false),
+        Question(R.string.question_oceans,true, false),
+        Question(R.string.question_mideast,false, false),
+        Question(R.string.question_africa,false, false),
+        Question(R.string.question_americas ,true, false),
+        Question(R.string.question_asia,true, false))
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,22 +32,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var trueButton:Button = findViewById(R.id.true_button)
-        var falseButton:Button = findViewById(R.id.false_button)
-        var nextButton:Button = findViewById(R.id.next_button)
-        var prevButton:Button = findViewById(R.id.prev_button)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
 
         questionTextView = findViewById(R.id.question_text_view)
 
-        trueButton.setOnClickListener { view: View ->
+        trueButton.setOnClickListener {
             /*Toast(this).setGravity(Gravity.TOP,0,0)
             Toast(this).show()*///хочу посмотреть как будет смещаться тост
             checkAnswer(true)
         }
-        falseButton.setOnClickListener { view: View ->
+        falseButton.setOnClickListener {
             checkAnswer(false)
         }
         nextButton.setOnClickListener {
+            if (currentIndex == 6) currentIndex = 0
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
@@ -76,13 +81,29 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onDestroy() called") }
 
     private fun updateQuestion() {
+
+        if (questionBank[currentIndex].choice == false) {
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true }
+        else {
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false}
+
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+
     }
 
+    private var percentageCorrect = 0
+
     private fun checkAnswer(userAnswer: Boolean) {
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
+        questionBank[currentIndex].choice = true
         val correctAnswer = questionBank[currentIndex].answer
-        val messageResId = if (userAnswer != correctAnswer) { R.string.incorrect_toast } else { R.string.correct_toast }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT) .show()
+        if (userAnswer != correctAnswer) {percentageCorrect+=0} else {percentageCorrect+=1}
+        val messageResId = if (userAnswer != correctAnswer) { getString(R.string.incorrect_toast) } else { getString(R.string.correct_toast) }
+        Toast.makeText(this, "$messageResId $percentageCorrect/6", Toast.LENGTH_SHORT ) .show()
+        //Toast.makeText(this, percentageCorrect.toString()+"/6" , Toast.LENGTH_SHORT ) .show()
     }
 }
--- app/src/main/java/com/bignerdrancho/android/geoquiz/Question.kt
+++ app/src/main/java/com/bignerdrancho/android/geoquiz/Question.kt
@@ -2,4 +2,4 @@ package com.bignerdrancho.android.geoquiz
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId: Int, val answer: Boolean)
+data class Question(@StringRes val textResId: Int, val answer: Boolean, var choice: Boolean)
